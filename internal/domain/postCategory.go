698d9d6b1e4b3fbc4f59ee32345fdd9a576d38ee
Arnald Gkiata <arnaldgjata@hotmail.com>
2025-07-07 01:11:22 +0300

    Remove unused files

--- internal/domain/postCategory.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-// Post-Category Model
-type PostCategory struct {
-	PostID     int64 `db:"post_id"`     // Foreign key to posts(id)
-	CategoryID int64 `db:"category_id"` // Foreign key to categories(id)
-}
