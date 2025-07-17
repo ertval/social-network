a90851f60624ec86da97c87355d6523251bb65fa
Arnald Gkiata <arnaldgjata@gmail.com>
2025-07-17 14:52:11 +0300

    Fix paths of reading frontend files

--- cmd/client/handler/handler.go
+++ cmd/client/handler/handler.go
@@ -17,7 +17,7 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
-	tmpl, err := template.ParseFiles("frontend/html/pages/home.html")
+	tmpl, err := template.ParseFiles("../../frontend/html/pages/home.html")
 	if err != nil {
 		log.Println("Error loading home.html:", err)
 		notFoundHandler(w, r, "Failed to load page", http.StatusInternalServerError)
@@ -32,7 +32,7 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func notFoundHandler(w http.ResponseWriter, _ *http.Request, errorMessage string, httpStatus int) {
-	tmpl, err := template.ParseFiles("frontend/html/pages/not_found.html")
+	tmpl, err := template.ParseFiles("../../frontend/html/pages/not_found.html")
 	if err != nil {
 		http.Error(w, errorMessage, httpStatus)
 		log.Println("Error loading not_found_page.html:", err)
