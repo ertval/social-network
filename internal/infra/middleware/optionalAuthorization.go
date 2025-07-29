8ff791a83e97750a402fdb185917535701caf278
geoikonomou <g.oikonomou.dev@gmail.com>
2025-07-29 23:31:49 +0300

    OptionalAuthorization Middleware

---
+++ internal/infra/middleware/optionalAuthorization.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/arnald/forum/internal/domain/user"
+)
+
+type optionalAuthMiddleware struct {
+	sessionManager user.SessionManager
+}
+
+type OptionalAuthMiddleware interface {
+	OptionalAuth(next http.Handler) http.Handler
+}
+
+func NewOptionalAuthMiddleware(sessionManager user.SessionManager) OptionalAuthMiddleware {
+	return optionalAuthMiddleware{
+		sessionManager: sessionManager,
+	}
+}
+func (a optionalAuthMiddleware) OptionalAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sessionID, err := r.Cookie("session_id")
+		if err != nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+		session, err := a.sessionManager.GetSession(sessionID.Value)
+		if err != nil || session == nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), userIDKey, session.UserID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
--- internal/infra/middleware/services.go
+++ internal/infra/middleware/services.go
@@ -6,10 +6,12 @@ import (
 
 type Middleware struct {
 	Authorization RequireAuthMiddleware
+	OptionalAuth  OptionalAuthMiddleware
 }
 
 func NewMiddleware(sessionManager user.SessionManager) *Middleware {
 	return &Middleware{
 		Authorization: NewRequireAuthMiddleware(sessionManager),
+		OptionalAuth:  NewOptionalAuthMiddleware(sessionManager),
 	}
 }
