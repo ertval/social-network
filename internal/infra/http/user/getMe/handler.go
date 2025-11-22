4ee0c023fa7081d6f458cfee2aa43b0d39d7e61d
nwntaspap <nwntaspapmichael@gmail.com>
2025-11-22 13:26:20 +0200

    create backend /me endpoint for retrieving current user data

---
+++ internal/infra/http/user/getMe/handler.go
@@ -0,0 +1,57 @@
+package getme
+
+import (
+	"net/http"
+
+	"github.com/arnald/forum/internal/infra/logger"
+	"github.com/arnald/forum/internal/infra/middleware"
+	"github.com/arnald/forum/internal/pkg/helpers"
+)
+
+type Handler struct {
+	logger logger.Logger
+}
+
+func NewHandler(logger logger.Logger) *Handler {
+	return &Handler{
+		logger: logger,
+	}
+}
+
+type GetMeResponse struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+// GetMe handler retrieves the current user from the session in the context
+func (h *Handler) GetMe(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		h.logger.PrintError(logger.ErrInvalidRequestMethod, nil)
+		helpers.RespondWithError(w, http.StatusMethodNotAllowed, "Invalid request method")
+		return
+	}
+
+	// Get user from context (set by middleware) using the helper function
+	user := middleware.GetUserFromContext(r)
+	if user == nil {
+		helpers.RespondWithError(w, http.StatusUnauthorized, "Unauthorized: User not found")
+		return
+	}
+
+	response := GetMeResponse{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+	}
+
+	helpers.RespondWithJSON(w, http.StatusOK, nil, response)
+
+	h.logger.PrintInfo(
+		"User retrieved from session",
+		map[string]string{
+			"userId": user.ID,
+			"name":   user.Username,
+		},
+	)
+}
