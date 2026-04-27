96ae93518255a0560f5ff7bd01507c68a274fee3
geoikonomou <g.oikonomou.dev@gmail.com>
2026-04-27 17:53:35 +0300

    add Client structure responsible for read/write from websocket to channel and in reverse

---
+++ internal/infra/http/ws/client.go
@@ -0,0 +1,91 @@
+package ws
+
+import (
+	"log"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 4096
+)
+
+type Client struct {
+	UserID string
+	hub    *Hub
+	conn   *websocket.Conn
+	send   chan []byte
+}
+
+func NewCient(userID string, hub *Hub, conn *websocket.Conn) *Client {
+	return &Client{
+		UserID: userID,
+		hub:    hub,
+		conn:   conn,
+		send:   make(chan []byte, 256),
+	}
+}
+
+// ReadPump pumps messages from webSocket to the hub.
+// Must run in its own goroutine.
+func (c *Client) ReadPump(onMessage func(client *Client, msg []byte)) {
+	defer func() {
+		c.hub.Unregister(c)
+		c.conn.Close()
+	}()
+
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error {
+		c.conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("ws read error: %v", err)
+			}
+			break
+		}
+		onMessage(c, msg)
+	}
+}
+
+// WritePump pumps messages from the send channel to the WebSocket.
+// Must run in its own goroutine.
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case msg, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			err := c.conn.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				return
+			}
+
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			err := c.conn.WriteMessage(websocket.PingMessage, nil)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+}
