80bd6f37402e1de5211da3f3c81f53514f62737f
danielkotsi <kotsi.a.daniel@gmail.com>
2026-05-22 01:45:36 +0300

    backend ready to recieve chat open-close events, now the typing needs to be handled

---
+++ internal/infra/ws/handlers/chatOpenHandler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"encoding/json"
+
+	ws "github.com/arnald/forum/internal/infra/ws"
+)
+
+type ChatOpenHandler struct {
+	hub *ws.Hub
+}
+
+func NewChatOpenHandler(hub *ws.Hub) *ChatOpenHandler {
+	return &ChatOpenHandler{
+		hub: hub,
+	}
+}
+
+func (h *ChatOpenHandler) Handle(client *ws.Client, env ws.Envelope) {
+	//to be implemented
+	var payload ws.ChatOpenClosePayload
+	if err := json.Unmarshal(env.Payload, &payload); err != nil {
+		sendError(client, env.RequestID, "invalid payload")
+		return
+	}
+
+	if payload.ChatID == "" {
+		sendError(client, env.RequestID, "chat_id and content are required")
+		return
+	}
+	h.hub.OpenChat(client, payload.ChatID)
+}
